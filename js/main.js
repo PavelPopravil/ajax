faceca4d01e737cc016a5abfc3898d77f7e437b2
Shishic <pasukmc@gmail.com>
2018-07-03 12:11:40 +0300

    add ajax request

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
 !function () {
     'use strict';
 
+    function initAjax(item) {
+        $.ajax({
+            methgod: 'GET',
+            dataType: 'json',
+            url: $(item).data('json'),
+            crossDomain: true
+        })
+        .success(function (data) {
+            console.log(data);
+        });
+    }
+
+    function setClickHandlers(item) {
+        item.on('click.linkTabs', function (e) {
+            e.preventDefault();
+            initAjax(this);
+        });
+    }
+
+    function initApp() {
+        var block = $('.link-tabs');
+        if (!block.length) {
+            return false;
+        }
+        var links = block.each(function (i, item) {
+           setClickHandlers(item);
+        });
+    }
+
+    $(function () {
+       initApp();
+    });
 }();
